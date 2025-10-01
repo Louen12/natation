b11dece17135f93836413d3ed36616972f2e0829
Theo22100 <theoguerinBZH@hotmail.com>
2025-10-01 09:25:49 +0200

    rename counter_page.dart to screens/counter_page.dart for better organization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'pages/counter_page.dart';
+import 'screens/counter_page.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
