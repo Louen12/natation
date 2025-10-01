b6a28c23164179d183adb9d2cc17647a277fb317
Théau's Arch Labtop <theaudugueperoux:git@gmail.com>
2025-10-01 10:06:42 +0200

    ADD jump and sleeving exercise pages

---
+++ lib/screens/jump_exercise.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JumpExercisePage extends StatefulWidget {
+  const JumpExercisePage({super.key});
+
+  @override
+  State<JumpExercisePage> createState() => _JumpExercisePageState();
+}
+
+class _JumpExercisePageState extends State<JumpExercisePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
