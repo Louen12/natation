e6525a173f7763cd1df3ebb502889fdb991512b5
Adrien ROBERT <adri312678@outlook.fr>
2025-10-19 16:33:43 +0200

    Connection to database

--- lib/repositories/exercise_service.dart
+++ lib/repositories/exercise_service.dart
@@ -7,10 +7,11 @@ class ExerciseProvider {
   late Database db;
 
   Future open(String path) async {
+    databaseFactory.deleteDatabase(path);
     db = await openDatabase(path, version: 1,
         onCreate: (Database db, int version) async {
           await db.execute('''
-          create table exercice ( 
+          create table exercise ( 
             id integer primary key autoincrement, 
             name text not null,
             reps integer not null,
@@ -24,24 +25,21 @@ class ExerciseProvider {
 
   Future<Exercise> insert(Exercise exercise) async {
     exercise.id = await db.insert("exercise", exercise.toMap());
-
     return exercise;
   }
 
   Future<List<Exercise>> getAllExercises() async {
+    List<Map<String, dynamic>> maps = await db.query('exercise');
+    return maps.map(Exercise.fromMap).toList();
+  }
+
+  Future fillDatabase() async{
     Exercise a = Exercise(name: "pompe", reps: 4, steps: 5, rest: 10, isFinished: false);
     Exercise b = Exercise(name: "abdo", reps: 3, steps: 6, rest: 10, isFinished: false);
     Exercise c = Exercise(name: "course", reps: 2, steps: 7, rest: 10, isFinished: false);
-    Map<String, dynamic> mapa = a.toMap();
-    Map<String, dynamic> mapb = b.toMap();
-    Map<String, dynamic> mapc = c.toMap();
-
-    List<Map<String, dynamic>> maps = [];
-    maps.add(mapa);
-    maps.add(mapb);
-    maps.add(mapc);
-    //List<Map<String, dynamic>> maps = await db.query('exercise');
-    return maps.map(Exercise.fromMap).toList();
+    insert(a);
+    insert(b);
+    insert(c);
   }
 
   Future close() async => db.close();
